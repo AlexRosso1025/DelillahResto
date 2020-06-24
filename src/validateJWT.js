2b5c4d7e4a12557b6219f22be9749b416972fd1b
Alex Rosso <alexrosso1025@gmail.com>
2020-06-24 00:27:38 -0500

    Se cambian mensajes y status

--- src/validateJWT.js
+++ src/validateJWT.js
@@ -3,21 +3,21 @@ const {key} = require('../src/config/jwt');
 const validateJWT = (req,res,next) =>{
     const token = req.header('token');
     if(!token){
-        res.status(401).json({message:`there's not a token in the request`});
+        res.status(401).json({message:`There's not a token in the request`});
     }
     try{
         const payload = jwt.verify(token,key);
         req.payload = payload;
         next();
     }catch(err){
-        res.status(401).json({message:`token isn't valid`});
+        res.status(401).json({message:`Token expired. Login again`});
     }
 };
 
 function validateAdmin (req,res,next){
     const{payload}=req;
     if(!payload.permit){
-        res.status(401).json({message:`you don't have permissions`});
+        res.status(403).json({message:`You don't have permissions`});
     }else{
         next();
     }
